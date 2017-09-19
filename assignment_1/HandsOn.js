adf10ebe1aee110a0b36235dc88af9fb23bf9a7b
Charley Ballantyne <charley@twin-one.com>
2017-09-19 09:45:09 -0700

    Assignment 1 Complete

---
+++ assignment_1/HandsOn.js
@@ -0,0 +1,34 @@
+//Excercise 1
+var name = "Charley";
+console.log("Hello " + name);
+ 
+//Excercise 2
+var x = 1125;
+var answer = x%2;
+console.log (answer);
+
+//Excercise 3
+var a = true;
+var b = false;
+var c = true;
+var or = a || b || c;
+console.log (or)
+
+//Excercise 4
+var and = a && b && c;
+console.log (and);
+
+//Excercise 5
+var answer1 = 10 - 3 *5/4 + 202 % 3;
+var answer2 = 10 - (3 * (5/4) + (202 % 3));
+console.log (answer1);
+console.log (answer2);
+
+//Excercise 6
+//var d = string(12);
+var e = parseInt("113");
+var f = parseFloat("1.12356");
+var x = parseInt("Hello World");
+console.log (x);
+
+
--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -2,8 +2,8 @@
 Exercise #1
 Initialize an empty array below in the variable numbers.
 */
-var numbers;
-console.log('Hello World');
+var numbers = new Array();
+
 
 /*
 Exercise #2
@@ -11,8 +11,8 @@ Using the loop provided for you below, use an array's push method to insert the 
 */
 
 for(var i=0;i<10;i++){
-    //use the array push() method here.
-}
+numbers.push (i)
+};
 
 
 /*
@@ -21,7 +21,7 @@ Test that you used the array push method correctly by printing the sum of the va
 The outputted value should be 9.
 */
 
-
+console.log (numbers[3]+numbers[6]);
 
 
 /*
@@ -29,12 +29,14 @@ Exercise #4
 Use the console.log() function and the array .pop() to add all the values in the array.
 */
 
+
+
 var sum = 0;
-/* remove this line to uncomment the code block
+
 while(numbers.length != 0){
-    //write pop function here
+    sum = sum + numbers.pop();
 }
-remove this line to uncomment the code block */
+
 console.log(sum);
 
 
@@ -43,11 +45,16 @@ Exercise #5
 Create an empty object and assign it to the variable car below.
 */
 var car;
+var car = new Object();
 
 /*
 Exercise #5
 Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputting the "colour" property to the console. You should see the value "black" printed out.
 */
 
+var car = {
+    colour:'black'
+};
 
+console.log(car.colour);
 
