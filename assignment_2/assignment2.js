3819dcd4090acca106a206ed71486b5fefaf62f6
Charley Ballantyne <charley@twin-one.com>
2017-09-19 10:09:49 -0700

    Assignment 2

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,7 +3,9 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
-
+for (var i = 1; i < 101; i++){
+    console.log(i)
+}
 
 /*
 Exercise #2
@@ -17,3 +19,7 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+for (var i=1; i<9; i++){
+    console.log(Array(i).join("#"))
+}
