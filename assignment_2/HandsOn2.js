413deba59ad1f671259294cf0ed167b0e45ae518
Charley Ballantyne <charley@twin-one.com>
2017-09-19 12:06:46 -0700

    Markup HTML Section complete.

---
+++ assignment_2/HandsOn2.js
@@ -0,0 +1,29 @@
+
+//Excercise #1
+
+/*
+for (var i=1; i<101; i++){
+    if (i % 5 == 0 && i % 3 == 0){
+        console.log("fizzbuzz");
+    }
+    else if (i % 5 == 0){
+        console.log("fizz");
+    }
+    else if (i % 3 == 0){
+        console.log("buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
+*/
+//Excercise #2
+
+for (var i = 1; i < 9; i++ ){
+    if (i % 2 == 0){
+        console.log(" # # # #");
+    }
+    else {
+        console.log("# # # # ")
+    }
+}
